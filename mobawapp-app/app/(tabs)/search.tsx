539b0aca2cb2dd06ed0805312e68564161481c1d
Rosalie Obeldobel <rosielisa2001@gmail.com>
2026-04-21 16:42:01 +0200

    feat(search): added handleSearch to get data from the apod server

--- mobawapp-app/app/(tabs)/search.tsx
+++ mobawapp-app/app/(tabs)/search.tsx
@@ -6,9 +6,36 @@ import React, { useState } from 'react';
 export default function Search() {
   const [fromDate, setFromDate] = useState(new Date())
   const [toDate, setToDate] = useState(new Date())
+  const [results, setResults] = useState([]); 
+  const [loading, setLoading] = useState(false);
 
-  const handleSavePress = (): void => {
+  const handleSearch = async () => {
+    setLoading(true);
     
+    const start = fromDate.toISOString().split('T')[0];
+    const end = toDate.toISOString().split('T')[0];
+    
+    const API_KEY = 'DEMO_KEY'; 
+    const url = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&start_date=${start}&end_date=${end}`;
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+  
+      const formattedData = data.map((item: any) => ({
+        id: item.date,           
+        name: item.title,      
+        description: item.explanation, 
+        image: item.url       
+      }));
+  
+      setResults(formattedData); 
+    } catch (error) {
+      console.error("API Error:", error);
+      alert("Fehler beim Abrufen der Daten!");
+    } finally {
+      setLoading(false); 
+    }
   };
 
   return (
@@ -23,7 +50,7 @@ export default function Search() {
       />
       <TouchableOpacity 
         style={Styles.touchable} 
-        onPress={handleSavePress} 
+        onPress={handleSearch} 
       >
         <Text style={Styles.touchabletext}>Search</Text>
       </TouchableOpacity>
