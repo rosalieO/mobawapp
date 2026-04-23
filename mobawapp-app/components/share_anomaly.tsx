2f9fbbbdf5b2ebdeb0a912276fbd3decbaef4081
Rosalie Obeldobel <rosielisa2001@gmail.com>
2026-04-23 20:09:01 +0200

    refactor: changed onShare so the user can send the local image or the url of the nasa-anomaly

--- mobawapp-app/components/share_anomaly.tsx
+++ mobawapp-app/components/share_anomaly.tsx
@@ -1,40 +1,56 @@
-import { View, TouchableOpacity, Share, Alert } from 'react-native';
+import * as Sharing from 'expo-sharing';
+import { Share, View, TouchableOpacity, Alert } from 'react-native';
 import { Styles } from '../constants/styles';
-import { Anomaly } from '../context/anomaly_context';
 import { MaterialIcons } from '@expo/vector-icons';
+import { Anomaly } from '../context/anomaly_context';
 
 interface ShareAnomalyProps {
     item: Anomaly
 }
 
-export function ShareAnomaly({item}: ShareAnomalyProps) {
+export function ShareAnomaly({ item }: ShareAnomalyProps) {
 
     const onShare = async () => {
       try {
-        const result = await Share.share({
-          message: `I've got the prettiest anomaly:3: ${item.name}\nDatum: ${item.id}\nBild: ${item.image}`,
-          title: 'NASA ANOMALY MONITOR', 
-        });
-    
-        if (result.action === Share.sharedAction) {
-          if (result.activityType) {
-          } else {
-          }
-        } else if (result.action === Share.dismissedAction) {
+        if (!item.image) {
+          Alert.alert("Info", "no image available");
+          return;
         }
+    
+        if (item.image.includes('http')) {
+          await Share.share({
+            message: `NASA Anomaly: ${item.name}\n\nImage: ${item.image}`,
+          });
+          return;
+        }
+
+        const isAvailable = await Sharing.isAvailableAsync();
+        
+        if (isAvailable) {
+          let cleanUri = item.image;
+          
+          if (cleanUri.includes(' ')) {
+            cleanUri = encodeURI(cleanUri);
+          }
+    
+          await Sharing.shareAsync(cleanUri);
+        } else {
+          await Share.share({
+            message: `Anomaly: ${item.name}\n(Lokal gespeichertes Bild)`,
+          });
+        }
+    
       } catch (error: any) {
-        Alert.alert(error.message);
+        console.error(error);
+        Alert.alert("Sharing Error", "this type of image cannot be shared currently!");
       }
     };
-    
+
     return (
         <View style={Styles.bottomrightcontainer}>
-          <TouchableOpacity 
-                style={Styles.sharebutton}
-                onPress={onShare}
-              >
+            <TouchableOpacity style={Styles.sharebutton} onPress={onShare}>
                 <MaterialIcons size={30} name="share" color='black' />
-          </TouchableOpacity> 
+            </TouchableOpacity>
         </View>
-    )
+    );
 }
